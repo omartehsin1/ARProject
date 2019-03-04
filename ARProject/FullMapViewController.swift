442e9c901274b3af26e4b863987097e6ad6afc2c
Omar Tehsin <omartehsin@gmail.com>
2019-03-04 11:28:21 -0500

    fixing go button bugs

--- ARProject/FullMapViewController.swift
+++ ARProject/FullMapViewController.swift
@@ -148,6 +148,7 @@ class FullMapViewController: UIViewController, UISearchBarDelegate {
     @IBAction func goButtonPressed(_ sender: Any) {
 
         getDirections()
+        //fixing button
     }
     
 
