055771903267c9d23e11340a32e55c9238e39099
Omar Tehsin <omartehsin@gmail.com>
2019-03-05 11:47:10 -0500

    added blue path to destination

--- ARProject/FullMapViewController.swift
+++ ARProject/FullMapViewController.swift
@@ -22,193 +22,186 @@ class FullMapViewController: UIViewController, UISearchBarDelegate {
     let geoCoder = CLGeocoder()
     var directionsArray: [MKDirections] = []
 
-    @IBOutlet weak var fullMapView: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        goButton.layer.cornerRadius = goButton.frame.size.height/2
         checkLocationServices()
-        
-        searchBarMap.delegate = self
-        
-        
-
-        // Do any additional setup after loading the view.
     }
     
-    func setupLocationManger() {
+    
+    
+    func setupLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        
-        
-        
     }
     
-    func centerViewOnUserLocation () {
+    
+    func centerViewOnUserLocation() {
         if let location = locationManager.location?.coordinate {
             let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-            fullMapView.setRegion(region, animated: true)
+            mapView.setRegion(region, animated: true)
         }
     }
+    
     
     func checkLocationServices() {
         if CLLocationManager.locationServicesEnabled() {
-            setupLocationManger()
+            setupLocationManager()
             checkLocationAuthorization()
         } else {
-            //show alert set up error handling after
+            // Show alert letting the user know they have to turn this on.
         }
     }
+    
+     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+     searchBarMap.resignFirstResponder()
+//     let geocoder = CLGeocoder()
+     geoCoder.geocodeAddressString(searchBarMap.text!) {
+     (placemarks:[CLPlacemark]?, error:Error?) in
+     if error == nil {
+     let placemark = placemarks?.first
+     let annotation = MKPointAnnotation()
+     annotation.coordinate = (placemark?.location?.coordinate)!
+     
+     annotation.title = self.searchBarMap.text!
+     
+     let span = MKCoordinateSpan(latitudeDelta: 0.075, longitudeDelta: 0.075)
+     let region = MKCoordinateRegion(center: annotation.coordinate, span: span)
+     
+     self.mapView.setRegion(region, animated: true)
+     
+     self.mapView.addAnnotation(annotation)
+     self.mapView.selectAnnotation(annotation, animated: true)
+     
+     } else {
+     print(error?.localizedDescription ?? "error")
+     }
+     }
+     
+     print("searching...\(searchBarMap.text!)")
+        getDirections()
+     
+     }
     
     func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
-            startTrackingUserLocation()
+            startTackingUserLocation()
         case .denied:
-            //show alert to turn on permissions
+            // Show alert instructing them how to turn on permissions
             break
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
         case .restricted:
-            //sent alert
+            // Show an alert letting them know what's up
             break
         case .authorizedAlways:
             break
-            
         }
     }
     
-    func startTrackingUserLocation() {
-        fullMapView.showsUserLocation = true
+    
+    func startTackingUserLocation() {
+        mapView.showsUserLocation = true
         centerViewOnUserLocation()
         locationManager.startUpdatingLocation()
-        previousLocation = getCentreLocation(for: fullMapView)
+        previousLocation = getCenterLocation(for: mapView)
     }
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBarMap.resignFirstResponder()
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(searchBarMap.text!) {
-            (placemarks:[CLPlacemark]?, error:Error?) in
-            if error == nil {
-                let placemark = placemarks?.first
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = (placemark?.location?.coordinate)!
-                
-                annotation.title = self.searchBarMap.text!
-                
-                let span = MKCoordinateSpan(latitudeDelta: 0.075, longitudeDelta: 0.075)
-                let region = MKCoordinateRegion(center: annotation.coordinate, span: span)
-                
-                self.fullMapView.setRegion(region, animated: true)
-                
-                self.fullMapView.addAnnotation(annotation)
-                self.fullMapView.selectAnnotation(annotation, animated: true)
-                
-            } else {
-                print(error?.localizedDescription ?? "error")
-            }
-        }
-        
-        print("searching...\(searchBarMap.text!)")
-        
-    }
     
-    func getCentreLocation(for mapView: MKMapView) -> CLLocation {
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
         let latitude = mapView.centerCoordinate.latitude
         let longitude = mapView.centerCoordinate.longitude
         
         return CLLocation(latitude: latitude, longitude: longitude)
     }
     
+    
     func getDirections() {
         guard let location = locationManager.location?.coordinate else {
+            //TODO: Inform user we don't have their current location
             return
         }
         
         let request = createDirectionsRequest(from: location)
         let directions = MKDirections(request: request)
         resetMapView(withNew: directions)
-        //find a way to implement live ETA that may hover over the AR Path
         
         directions.calculate { [unowned self] (response, error) in
-            guard let response = response else { return }
+            //TODO: Handle error if needed
+            guard let response = response else { return } //TODO: Show response not available in an alert
             
             for route in response.routes {
-                self.fullMapView.addOverlay(route.polyline)
-                self.fullMapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true )
+                self.mapView.addOverlay(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
             }
         }
-        
     }
+    
+    
     func createDirectionsRequest(from coordinate: CLLocationCoordinate2D) -> MKDirections.Request {
-        let destinationCoordinate = getCentreLocation(for: fullMapView).coordinate
-        let startingLocation = MKPlacemark(coordinate: coordinate)
-        let destination = MKPlacemark(coordinate: destinationCoordinate)
+        let destinationCoordinate       = getCenterLocation(for: mapView).coordinate
+        let startingLocation            = MKPlacemark(coordinate: coordinate)
+        let destination                 = MKPlacemark(coordinate: destinationCoordinate)
         
-        let request = MKDirections.Request()
-        request.source = MKMapItem(placemark: startingLocation)
-        request.destination = MKMapItem(placemark: destination)
-        request.transportType = .walking
-        request.requestsAlternateRoutes = false
+        let request                     = MKDirections.Request()
+        request.source                  = MKMapItem(placemark: startingLocation)
+        request.destination             = MKMapItem(placemark: destination)
+        request.transportType           = .walking
+        request.requestsAlternateRoutes = true
         
         return request
     }
     
+    
     func resetMapView(withNew directions: MKDirections) {
-        fullMapView.removeOverlay(fullMapView.overlays as! MKOverlay)
+        mapView.removeOverlays(mapView.overlays)
         directionsArray.append(directions)
-        let _ = directionsArray.map { $0.cancel()}
+        let _ = directionsArray.map { $0.cancel() }
     }
     
-    @IBAction func goButtonPressed(_ sender: Any) {
-
+    
+    @IBAction func goButtonTapped(_ sender: UIButton) {
         getDirections()
-        //fixing button
     }
-    
-
-
 }
 
+
 extension FullMapViewController: CLLocationManagerDelegate {
-    
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        guard let location = locations.last else { return }
-//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-//        fullMapView.setRegion(region, animated: true)
-//
-//    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
     }
-    
 }
+
 
 extension FullMapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        let centre = getCentreLocation(for: mapView)
-        let geoCoder = CLGeocoder()
+        let center = getCenterLocation(for: mapView)
+        
         guard let previousLocation = self.previousLocation else { return }
         
-        guard centre.distance(from: previousLocation) > 50 else { return }
-        self.previousLocation = centre
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
         
         geoCoder.cancelGeocode()
         
-        geoCoder.reverseGeocodeLocation(centre) { [weak self] (placemarks, error) in
-            guard let self = self else {return }
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
             
             if let _ = error {
+                //TODO: Show alert informing the user
                 return
             }
             
             guard let placemark = placemarks?.first else {
+                //TODO: Show alert informing the user
                 return
             }
+            
             let streetNumber = placemark.subThoroughfare ?? ""
             let streetName = placemark.thoroughfare ?? ""
             
@@ -217,10 +210,44 @@ extension FullMapViewController: MKMapViewDelegate {
             }
         }
     }
-    func mapView(_ mapView: MKMapView, redererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
-        renderer.strokeColor = UIColor.blue
+        renderer.strokeColor = .blue
         
         return renderer
     }
 }
+
+
+/*
+ func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+ searchBarMap.resignFirstResponder()
+ let geocoder = CLGeocoder()
+ geocoder.geocodeAddressString(searchBarMap.text!) {
+ (placemarks:[CLPlacemark]?, error:Error?) in
+ if error == nil {
+ let placemark = placemarks?.first
+ let annotation = MKPointAnnotation()
+ annotation.coordinate = (placemark?.location?.coordinate)!
+ 
+ annotation.title = self.searchBarMap.text!
+ 
+ let span = MKCoordinateSpan(latitudeDelta: 0.075, longitudeDelta: 0.075)
+ let region = MKCoordinateRegion(center: annotation.coordinate, span: span)
+ 
+ self.mapView.setRegion(region, animated: true)
+ 
+ self.mapView.addAnnotation(annotation)
+ self.mapView.selectAnnotation(annotation, animated: true)
+ 
+ } else {
+ print(error?.localizedDescription ?? "error")
+ }
+ }
+ 
+ print("searching...\(searchBarMap.text!)")
+ 
+ }
+ */
